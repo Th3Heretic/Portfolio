10eb81a6c2069edccc8c020cd494fea31ad4da98
Th3Heretic <146324718+Th3Heretic@users.noreply.github.com>
2023-10-12 09:40:21 +0100

    Exercises

---
+++ Week 1/Homework/Exercises.py
@@ -0,0 +1,15 @@
+#Hello World
+print('Hello World')
+
+#Hello Damien
+print('Hello Damien')
+
+#Temperature Converter
+# Temperature in celsius degree
+celsius = (input)
+
+fahrenheit = float(celsius * 1.8) + 32
+
+# printing the result
+print('Celsius is equivalent to: Fahrenheit'
+	% (celsius, fahrenheit))
