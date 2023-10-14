1358693a02d607793f82d08a984261d4d4237e3a
Th3Heretic <146324718+Th3Heretic@users.noreply.github.com>
2023-10-14 21:30:41 +0100

    Completed task 1

---
+++ Week 2/Task/VariableAssignment.py
@@ -0,0 +1,16 @@
+
+# Assigning a value to a variable
+total = 100
+
+print("The total is:", total)
+
+# Adding a value to the existing variable
+total = total + 99
+
+print("And now the total is:", total)
+
+# Augmented assignment (This is a shorthand way to write the above expression)
+total += 99
+
+print("Using the augmented assigment, I've added another 99:", total)
+
