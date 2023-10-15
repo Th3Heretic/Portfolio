77bfcc7e0ab91d3f601b7e33dc54c08cf0357fda
Th3Heretic <146324718+Th3Heretic@users.noreply.github.com>
2023-10-15 12:31:02 +0100

    Finished task 5

---
+++ Week 2/Task/CallingBuiltInFunctions.py
@@ -0,0 +1,16 @@
+
+# The print function is built-in
+
+# Using the 'len' built-in function to determine the length of a string
+name_length = len("The quick brown fox jumped over the lazy dog")
+
+print(name_length)
+
+# Asking your name and telling you how long it is
+name = input('What is your name? ')
+print('Hi there', name, 'The length of your name is', len(name), 'characters long')
+
+# Asking your age and telling you how old you will be in a year
+age = int(input("What is your age? "))
+
+print("You are", age, "years old and in a year you will be", age+1)
