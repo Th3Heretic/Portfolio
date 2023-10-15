dee480f4c1c846e25d50283cb313e3f3ba02c526
Th3Heretic <146324718+Th3Heretic@users.noreply.github.com>
2023-10-15 12:31:19 +0100

    Finished task 6

---
+++ Week 2/Task/UserInputSemantics.py
@@ -0,0 +1,6 @@
+
+# Getting two inputs from the user and multiplying them together
+num1 = int(input("Pick a whole number: "))
+num2 = int(input("Now pick another: "))
+
+print("Your numbers multiplied is:", num1*num2)
