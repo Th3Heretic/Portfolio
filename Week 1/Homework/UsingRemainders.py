4ceb1907bbdaf11c29da7fad5606c7abf57e2530
Th3Heretic <146324718+Th3Heretic@users.noreply.github.com>
2023-10-17 15:26:13 +0100

    Corrected Syntax

--- Week 1/Homework/UsingRemainders.py
+++ Week 1/Homework/UsingRemainders.py
@@ -3,4 +3,4 @@ number = [113, 175, 12]
 for i in range (3):
     remainder = number[i] % 24
     divide = number[i] // 24
-    print(number[i], "number of students will make", divide, "filled groups and", remainder, "students left over")
+    print(number[i], "students will make", divide, "filled groups and", remainder, "students left over")
