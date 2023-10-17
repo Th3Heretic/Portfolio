2bc57f9d8f1c31b09219d29cc654cf06a92009d1
Th3Heretic <146324718+Th3Heretic@users.noreply.github.com>
2023-10-17 15:23:15 +0100

    Finished Task 1

---
+++ Week 3/Task/BooleanExpressions.py
@@ -0,0 +1,12 @@
+
+# Types of Boolean expressions
+print(10<100)
+print(100!=100)
+print(50>=50)
+
+# Are you young or not?
+age = int(input("How old are you?: "))
+
+print(int(age<18))
+print(int(age<21))
+print(int(age<31))
