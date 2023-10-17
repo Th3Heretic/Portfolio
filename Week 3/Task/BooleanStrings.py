f328332a841d4744b61706981b0f74e4d639b1af
Th3Heretic <146324718+Th3Heretic@users.noreply.github.com>
2023-10-17 15:23:24 +0100

    Finished Task 2

---
+++ Week 3/Task/BooleanStrings.py
@@ -0,0 +1,10 @@
+
+# Using Booleans to compare other data types
+print('a < b =', "a" < "b")
+print('b < a =', "b" < "a")
+
+# Comparing names using booleans
+print('John < Terry =', "John" < "Terry")
+
+print('john < Terry =', "john" < "Terry")
+
