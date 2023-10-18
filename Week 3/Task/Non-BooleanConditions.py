eff589c4b5a4306cab3b6782ce9d2b445102956c
Th3Heretic <146324718+Th3Heretic@users.noreply.github.com>
2023-10-18 16:21:37 +0100

    Finished task 8

---
+++ Week 3/Task/Non-BooleanConditions.py
@@ -0,0 +1,9 @@
+
+# Non-Booleans can be used to express conditions that don't equate to a true or false
+name = input("What is your name? ")
+
+if name:
+    print("Your name is", name)
+    print("Nice to meet you,", name)
+else:
+    print("You have not entered anything")
