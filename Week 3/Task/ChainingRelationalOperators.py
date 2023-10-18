2b5928e322736dd17ab438f756358b1bd0de7bd6
Th3Heretic <146324718+Th3Heretic@users.noreply.github.com>
2023-10-18 13:03:52 +0100

    Finished task 5

---
+++ Week 3/Task/ChainingRelationalOperators.py
@@ -0,0 +1,15 @@
+
+# You can use chaining to replace the 'and' operator
+age = int(input("How old are you? "))
+
+print(18 < age and age <=65)
+# This is equivalent to:
+print(18 < age <= 65)
+
+# Chaining task example
+height = float(input("How tall are you(cm)? "))
+weight = float(input("How much do you weigh(Kg)? "))
+
+print(131 < height < 190)
+print(50 < weight < 120)
+
