cf120daf8c46c24b1e380e6c88f635817b049925
Th3Heretic <146324718+Th3Heretic@users.noreply.github.com>
2023-10-18 16:50:49 +0100

    Finished Task 10

---
+++ Week 3/Task/AreYouAnAdult?.py
@@ -0,0 +1,6 @@
+
+# Using a ternary operation to determine if you are an adult
+age = int(input("How old are you? "))
+
+adult = print("You are a grown up") if age >= 18 else print("You are not a grown up yet")
+
