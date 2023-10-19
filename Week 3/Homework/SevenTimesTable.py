a55f6fb28894d225142ed3a5eb07551e38d3c88c
Th3Heretic <146324718+Th3Heretic@users.noreply.github.com>
2023-10-19 09:21:08 +0100

    progress

--- Week 3/Homework/SevenTimesTable.py
+++ Week 3/Homework/SevenTimesTable.py
@@ -3,3 +3,8 @@ num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
 for num in range(13):
     print(num, "X 7 =", num * 7)
+
+choice = int(input("Pick a number to multiply by 7: "))
+
+for num in range(choice):
+    print(num, "X 7 =", choice * 7)
