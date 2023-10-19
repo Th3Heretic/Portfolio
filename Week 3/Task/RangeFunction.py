b8c7ede7b62dd2b81f6093a5506aef59b6ce8c56
Th3Heretic <146324718+Th3Heretic@users.noreply.github.com>
2023-10-19 09:14:41 +0100

    Finished Task 12

---
+++ Week 3/Task/RangeFunction.py
@@ -0,0 +1,13 @@
+
+# Using the for loop for numbers
+#for n in range(5):
+#    print(n, "doubled is", n * 2)
+
+#for n in range(10, 20):
+#    print(n, "doubled is", n * 2)
+
+#for n in range(20, 0, -1):
+#    print(n, "doubled is", n * 2)
+
+for n in range(0, 11, 2):
+    print(n, "to the power of itself is", n**n)
