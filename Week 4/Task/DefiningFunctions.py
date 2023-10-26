18bc5717813cbd003deb967f434af1f8de7ec0a0
Th3Heretic <146324718+Th3Heretic@users.noreply.github.com>
2023-10-26 07:58:45 +0100

    Updated task 2 (Defining Functions to include use of docstrings)

--- Week 4/Task/DefiningFunctions.py
+++ Week 4/Task/DefiningFunctions.py
@@ -2,10 +2,12 @@
 # instead of rewriting it separately
 msg = "Executed correctly"
 
-
 def displaytwice(msg):
+    """Displays a message twice upon execution"""
     print(msg)
     print(msg)
 
 
 displaytwice(msg)
+# Demonstrates how to use and retrieve docstrings
+print(help(displaytwice))
