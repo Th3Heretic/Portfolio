e1dea0b50b26ab6a553eea2040768e24c6a1e50a
Th3Heretic <146324718+Th3Heretic@users.noreply.github.com>
2023-10-30 17:22:10 +0000

    Updated

--- Week 2/Practical/UsingDifferentQuotationMarks.py
+++ Week 2/Practical/UsingDifferentQuotationMarks.py
@@ -1,6 +1,6 @@
 
 # An example of using only one type of quote marks
-print('Oh, so that's what you 'thought' then is it?')
+#print('Oh, so that's what you 'thought' then is it?')
 
 # The use of different quote marks allows you to make a grammatically correct string with appropriate punctuation
-print("Oh, so that's what you 'thought' then is it?!")
+#print("Oh, so that's what you 'thought' then is it?!")
